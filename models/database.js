fe509e61a9e1a688e996f5b35805cbaa1e7bbb99
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-11-08 14:48:03 +0100

    feat: Addd test file and environment

--- models/database.js
+++ models/database.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+// Load the URI from the environment variables defined in the .env file
+const dbURI = process.env.MONGODB_URI;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function(){
-    console.log('Mongodb Database connected')
-})
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+});
 
 //Models
 
---
+++ tests/auth.test.js
@@ -0,0 +1,33 @@
+const propertyController = require('../controllers/propertyController'); // Import your property controller
+const Property = require('../models/Property'); // Import your Property model
+
+jest.mock('../models/Property', () => ({
+  find: jest.fn(),
+  findById: jest.fn(),
+  findByIdAndUpdate: jest.fn(),
+  findByIdAndRemove: jest.fn(),
+}));
+
+describe('Property Controller Tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks(); // Clear mock function calls between tests
+  });
+
+  it('should get all properties', async () => {
+    const mockProperties = [{ _id: '1', name: 'Property 1' }, { _id: '2', name: 'Property 2' }];
+    Property.find.mockResolvedValue(mockProperties);
+
+    const req = {};
+    const res = {
+      json: jest.fn(),
+      status: jest.fn(),
+    };
+
+    await propertyController.getAllProperties(req, res);
+
+    expect(Property.find).toHaveBeenCalled();
+    expect(res.json).toHaveBeenCalledWith(mockProperties);
+  });
+
+  // Similarly, write tests for other functions like createProperty, getPropertyById, updateProperty, deleteProperty, and searchProperties
+});
